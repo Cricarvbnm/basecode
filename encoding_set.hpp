49bfe2015ba4ff2158164f832a5f677bc536d343
Cricar <yml1457086128@163.com>
2024-02-12 16:34:51 +0800

    Add a member into `EncodingSet` to store pad character
    The type of the member is `std::optional<char>`.
    If it has value, the encoding contain pad.

--- encoding_set.hpp
+++ encoding_set.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <optional>
 
 namespace basecode::_ {
 
@@ -8,7 +9,9 @@ template <int set_len> class EncodingSet {
   const std::array<char, set_len> set;
 
 public:
-  EncodingSet(const std::array<char, set_len> &set) : set(set) {}
+  EncodingSet(const std::array<char, set_len> &set,
+              std::optional<char> pad = std::nullopt)
+      : set(set), pad(pad) {}
 
   int find(char chr) const noexcept {
     for (int i = 0; i < set_len; ++i)
@@ -17,6 +20,8 @@ public:
 
     return -1;
   }
+
+  const std::optional<char> pad;
 
   char operator[](size_t i) const noexcept { return set[i]; }
   char at(size_t i) const { return set.at(i); }
